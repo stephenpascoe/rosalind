d88c19d9fbbc4cee9900daafb00245603774281b
Stephen Pascoe <Stephen.Pascoe@nanoporetech.com>
2018-03-29 15:08:29 +0100

    Refactor into one executable.

---
+++ app/Main.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import System.Environment
+import Data.Maybe
+import qualified Data.ByteString.Lazy.Char8 as BS
+
+import Problems
+
+main :: IO ()
+main = do
+  args <- getArgs
+  input <- BS.getContents
+  let result = do problemStr <- safeHead args
+                  problem <- selectProblem problemStr
+                  return $ problem input
+  case result of
+    Just output -> BS.putStrLn output
+    Nothing -> BS.putStrLn "Problem not recognised"
+
+
+
+
+selectProblem :: String -> Maybe Problem
+selectProblem "dna" = Just dna
+selectProblem "rna" = Just rna
+selectProblem "recv" = Just recv
+selectProblem _      = Nothing
+
+
+safeHead :: [a] -> Maybe a
+safeHead [] = Nothing
+safeHead (a:_) = Just a
--- src/Problems.hs
+++ src/Problems.hs
@@ -1,6 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Main where
+module Problems
+    ( dna
+    , rna
+    , recv
+    , Problem
+    ) where
 
 import qualified Data.ByteString.Lazy.Char8 as BS
 import qualified Data.ByteString.Builder as B
@@ -8,16 +13,19 @@ import Data.Monoid
 
 import Lib
 
-main :: IO ()
-main = do
-  input <- BS.getContents
-  BS.putStrLn $ dna input
+type Problem = BS.ByteString -> BS.ByteString
 
-
-dna :: BS.ByteString -> BS.ByteString
+dna :: Problem
 dna input = let counts = countBases input
             in B.toLazyByteString ( B.integerDec (aCount counts) <> " " <>
                                     B.integerDec (cCount counts) <> " " <>
                                     B.integerDec (gCount counts) <> " " <>
                                     B.integerDec (tCount counts)
                                   )
+
+rna :: Problem
+rna input = BS.map transcribe input
+
+recv :: Problem
+recv input = let strands = BS.words input
+             in  BS.reverse (BS.map complement (head strands))
