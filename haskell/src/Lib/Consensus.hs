33d60dcafac8427e1d8ae4a944c97af1babb981d
root <root@71823d7bd422>
2020-08-28 09:27:50 +0000

    WIP

--- haskell/src/Lib/Consensus.hs
+++ haskell/src/Lib/Consensus.hs
@@ -1,16 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Consensus ( fastaToProfileMatrix
-                 )
+module Lib.Consensus ( fastaToProfileMatrix
+           	     )
 where
 
 import Data.List
 
-import qualified Vector as V
+import qualified Data.Vector as V
 import qualified Lib.Fasta as F
 
 
-data ProfileMatrix = ProfileMatrix { lengthPM :: int
+data ProfileMatrix = ProfileMatrix { lengthPM :: Int
                                    , countA :: V.Vector Int
                                    , countC :: V.Vector Int
                                    , countG :: V.Vector Int
@@ -24,25 +24,19 @@ initPM n = ProfileMatrix n f f f f where
   f = V.replicate n 0
 
 
-addPM :: ProfileMatrix -> ProfileMatrix -> Either String ProfileMatrix
-addPM pm1 pm2 =
-  let
-    addPM' = ProfileMatrix { countA = f (countA pm1) (countA pm2)
-                           , countC = f (countC pm1) (countC pm2)
-                           , countG = f (countG pm1) (countG pm2)
-                           , countT = f (countT pm1) (countT pm2)
-                           }
-    f v1 v2 = V zipWith (+) v1 v2
-  in
-    case (lengthPM pm1) /= (lengthPM pm2) of
-      True -> Left "ProfileMatrixes are not the same length"
-      False -> Right addPM' pm1 pm2
+addPM :: ProfileMatrix -> ProfileMatrix -> ProfileMatrix
+addPM pm1 pm2 = ProfileMatrix { countA = f (countA pm1) (countA pm2)
+       	                      , countC = f (countC pm1) (countC pm2)
+                              , countG = f (countG pm1) (countG pm2)
+                              , countT = f (countT pm1) (countT pm2)
+                              } where
+		  f v1 v2 = V.zipWith (+) v1 v2
 
 
 fastaToProfileMatrix :: F.Fasta -> ProfileMatrix
 fastaToProfileMatrix fasta =
   let
     entries = F.entries fasta
-    pm0 = intPM $ length entries
+    pm0 = initPM $ length entries
   in
     foldr addPM pm0 entries
